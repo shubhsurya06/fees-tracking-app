b92c705b149000892d451f2363194b4f1a1d99d7
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-10-31 09:29:52 +0530

    project structure created

---
+++ src/app/core/constant/constant.ts
@@ -0,0 +1,11 @@
+export const ApiConstant = {
+    API_TYPES: {
+        MASTER: 'Master',
+        USER: 'User'
+    },
+    MASTER_APIS: {
+        GET_ALL_MASTER: 'get-all-master',
+        CREATE_MASTER: 'create-master',
+        UPDATE_MASTER: 'update-master'
+    }
+}
---
+++ src/app/core/model/user-model.ts
@@ -0,0 +1,9 @@
+export interface User {
+    "userId": string;
+    "userName": string;
+    "password": string;
+    "role": string;
+    "instituteId": number;
+    "studentId": number;
+    "branchId": number;
+}
