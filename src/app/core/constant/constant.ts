300d407d12be80cbb8c63023254c3d07efdc2bdf
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-11-02 02:32:06 +0530

    master page done

--- src/app/core/constant/constant.ts
+++ src/app/core/constant/constant.ts
@@ -4,9 +4,11 @@ export const ApiConstant = {
         USER: 'User'
     },
     MASTER_APIS: {
-        GET_ALL_MASTER: '/get-all-master',
+        GET_ALL_MASTER: '/get-all-masters',
         CREATE_MASTER: '/create-master',
-        UPDATE_MASTER: '/update-master'
+        UPDATE_MASTER: '/update-master',
+        DELETE_MASTER: '/delete-master',
+        GET_MASTER_BY_TYPE: '/get-masters-by-type'
     },
     USER_APIS: {
         LOGIN: '/login',
--- src/app/core/services/master/master-service.ts
+++ src/app/core/services/master/master-service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { ApiConstant } from '../../constant/constant';
+import { Observable } from 'rxjs';
+import { IMaster } from '../../model/master-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MasterService {
-  
+
+  private baseUrl: string = environment.API_URL;
+
+  http = inject(HttpClient);
+
+  getAllMasters() {
+    let url = this.baseUrl + ApiConstant.API_TYPES.MASTER + ApiConstant.MASTER_APIS.GET_ALL_MASTER;
+    return this.http.get(url);
+  }
+
+  createMaster(req: IMaster) {
+    let url = this.baseUrl + ApiConstant.API_TYPES.MASTER + ApiConstant.MASTER_APIS.CREATE_MASTER;
+    return this.http.post(url, req);
+  }
+
+  updateMaster(req: IMaster) {
+    let url = this.baseUrl + ApiConstant.API_TYPES.MASTER + ApiConstant.MASTER_APIS.UPDATE_MASTER;
+    return this.http.put(url + '/' + req.masterId, req);
+  }
+
+  deleteMaster(id: number) {
+    let url = this.baseUrl + ApiConstant.API_TYPES.MASTER + ApiConstant.MASTER_APIS.DELETE_MASTER;
+    return this.http.delete(url + '/' + id);
+  }
+
+  getMasterByType(masterFor: string): Observable<IMaster[]> {
+    let url = this.baseUrl + ApiConstant.API_TYPES.MASTER + ApiConstant.MASTER_APIS.GET_MASTER_BY_TYPE;
+    return this.http.get<IMaster[]>(url + '/' + masterFor);
+  }
 }
