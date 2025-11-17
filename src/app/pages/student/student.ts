4638a68504078f0f12a091bdd106a28b39e03474
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-11-17 23:59:12 +0530

    CRUD student

--- src/app/pages/student/student.ts
+++ src/app/pages/student/student.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Output, inject, signal } from '@angular/core';
-import { ICourses } from '../../core/model/course-model';
-import { CourseService } from '../../core/services/course/course-service';
 import { APP_CONSTANT } from '../../core/constant/appConstant';
-import { DatePipe, NgClass } from '@angular/common';
+import { DatePipe, NgClass, NgFor } from '@angular/common';
 import { UserService } from '../../core/services/user/user-service';
 import { ReactiveFormsModule, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { IInstituteModel } from '../../core/model/institute-model';
@@ -11,10 +9,12 @@ import { AlertBox } from '../../shared/reusableComponent/alert-box/alert-box';
 import { IAlert } from '../../core/model/alert-model';
 import { IStudent } from '../../core/model/student-model';
 import { StudentService } from '../../core/services/student/student-service';
+import { IMaster } from '../../core/model/master-model';
+import { MasterService } from '../../core/services/master/master-service';
 
 @Component({
   selector: 'app-student',
-  imports: [AlertBox],
+  imports: [AlertBox, ReactiveFormsModule, NgClass, NgFor],
   templateUrl: './student.html',
   styleUrl: './student.scss'
 })
@@ -27,22 +27,60 @@ export class Student implements OnInit {
   commonService = inject(CommonService);
   studentService = inject(StudentService);
   isStudentListLoading = signal<boolean>(false);
-  isAddUpdateLoader = signal<boolean>(false);
+  isAddEditStudLoader = signal<boolean>(false);
   studentList = signal<IStudent[]>([]);
   instituteAdminRole = APP_CONSTANT.USER_ROLES.INSTITUTE_ADMIN;
   instituteList = signal<IInstituteModel[]>([]);
+  refByMasterList = signal<IMaster[]>([]);
+  masterService = inject(MasterService);
   studentForm!: FormGroup;
+  fb = inject(FormBuilder);
   isAddEditStudentLoader = signal<boolean>(false);
-
 
   constructor() {
     if (!Object.keys(this.userService.loggedInUser()).length) {
       this.userService.getLoggedInUser();
     }
+
+    this.studentForm = this.fb.group({
+      studentId: [0, Validators.required],
+      refrenceById: [0, Validators.required],
+      instituteId: [0, Validators.required],
+      name: ['', Validators.required],
+      contactNo: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      pincode: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+      qualification: ['', Validators.required],
+      collegeName: ['', Validators.required],
+      collegeCity: ['', Validators.required],
+      familyDetails: ['', Validators.required],
+      aadharCard: ['', Validators.required],
+      profilePhotoName: ['', Validators.required]
+    })
+
+    this.studentForm.controls['instituteId'].setValue(this.userService.loggedInUser().instituteId);
+
   }
 
   ngOnInit(): void {
+    this.getMasterByReference();
     this.getStudentByInstitute();
+  }
+
+  /**
+   * get master list for 'Reference By' type from masterService on page load
+   */
+  getMasterByReference() {
+    this.masterService.getMasterByType('Reference By').subscribe({
+      next: (res: any) => {
+        this.refByMasterList.set(res?.data ?? []);
+      },
+      error: (error) => {
+        console.log('Some error while loading master by ref List in enrollments:', error);
+      }
+    });
   }
 
   /**
@@ -65,11 +103,127 @@ export class Student implements OnInit {
     })
   }
 
-  editStudent(student: IStudent) {
-    
+  /**
+   * Perform actions when Add/Update student is success
+   * @param studentData 
+   */
+  private onAddEditStudentSuccess(studentData: any) {
+    this.isAddEditStudLoader.set(false);
+    let res: IStudent = studentData.data;
+    this.studentForm.reset();
+
+    try {
+      this.closeModal();
+    } catch (e) {
+      // ignore if bootstrap not available
+    }
+    this.cancelEdit();
+
+    // Update the branch list
+    this.studentList.update(list => {
+      const index = list.findIndex(b => b.studentId === res.studentId);
+      if (index === -1) {
+        return [...list, res];
+      } else {
+        list[index] = res;
+        return [...list];
+      }
+    })
   }
 
-  deleteStudent(id: number) {
+  /**
+   * Perform actions when Add/Edit student having any error
+   * @param err 
+   */
+  handleStudentAddEditError(err: any) {
+    this.isAddEditStudLoader.set(false);
+    alert(err.message);
+  }
 
+  /**
+   * Add/Update new student from here
+   * @returns 
+   */
+  createStudent() {
+    if (this.studentForm.invalid) {
+      alert('Please enter all fields of form.')
+      return;
+    }
+
+    console.log('Student form details:', this.studentForm.value);
+
+    this.isAddEditStudLoader.set(true);
+    const studentData: IStudent = this.studentForm.value;
+
+    if (studentData.studentId) {
+      // Update existing branch
+      this.studentService.updateStudent(studentData).subscribe({
+        next: (res: any) => {
+          this.onAddEditStudentSuccess(res);
+        },
+        error: this.handleStudentAddEditError.bind(this)
+      });
+    } else {
+      // Create new branch
+      this.studentService.createStudent(studentData).subscribe({
+        next: (res: any) => {
+          this.onAddEditStudentSuccess(res);
+        },
+        error: this.handleStudentAddEditError.bind(this)
+      });
+    }
+  }
+
+  /**
+   * Set student data from here when click on edit student button
+   * @param student 
+   */
+  editStudent(student: IStudent) {
+    this.studentForm.patchValue(student);
+  }
+
+  /**
+   * When student is in edit mode and student cancels edit, then perform form reset
+   */
+  cancelEdit() {
+    this.studentForm.reset();
+  }
+
+  /**
+   * delete student data from here and update studentList
+   * @param id 
+   * @returns 
+   */
+  deleteStudent(id: number) {
+    if (!id) return;
+
+    const isConfirmed = confirm('Are you sure you want to delete this Student?');
+    if (!isConfirmed) return;
+
+    this.studentService.deleteStudent(id).subscribe({
+      next: (res: any) => {
+        this.studentList.update(list => list.filter(b => b.studentId !== id));
+      },
+      error: (error: any) => {
+        console.error('Error deleting branch:', error);
+      }
+    });
+  }
+
+  /**
+   * Close bootstrap modal with id 'staticBackdrop' programmatically.
+   */
+  private closeModal() {
+    const modalEl = document.getElementById('studentModalId');
+    if (!modalEl) return;
+    const bootstrap = (window as any).bootstrap;
+    if (bootstrap && bootstrap.Modal) {
+      const inst = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
+      inst.hide();
+      return;
+    }
+    // fallback: click close button if modal API not found
+    const closeBtn = modalEl.querySelector('.btn-close') as HTMLElement | null;
+    closeBtn?.click();
   }
 }
